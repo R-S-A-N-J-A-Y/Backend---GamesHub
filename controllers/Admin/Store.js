efc6f25c6bff59dba21d5712abd2c5dcff743547
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-10 10:34:29 +0530

    Added the Get request to stores and filtered the name and url using npm package lodash.

--- controllers/Admin/Store.js
+++ controllers/Admin/Store.js
@@ -1,5 +1,18 @@
 const { StoreModel, Validate } = require("../../models/StoreModel");
+const _ = require("lodash");
 
+// get all the stores name and url
+exports.getAll = async () => {
+  try {
+    let data = await StoreModel.find();
+    data = data.map((obj) => _.pick(obj, ["name", "url"]));
+    return { success: true, data: data };
+  } catch (err) {
+    return { success: false, message: `${err.code} ${err.errmsg}` };
+  }
+};
+
+// Add new Store details to Db
 exports.addStore = async (data) => {
   const { error } = Validate(data);
   if (error)
--- routes/Admin/Stores.js
+++ routes/Admin/Stores.js
@@ -2,6 +2,14 @@ const express = require("express");
 const routes = express.Router();
 const StoreController = require("../../controllers/Admin/Store");
 
+//GET - to get all the stores
+routes.get("/", async (req, res) => {
+  const result = await StoreController.getAll();
+  if (!result.success) return res.status(500).send({ message: result.message });
+  return res.send({ data: result });
+});
+
+// POST - Create an New Stores
 routes.post("/", async (req, res) => {
   const result = await StoreController.addStore(req.body);
   if (!result.success)
