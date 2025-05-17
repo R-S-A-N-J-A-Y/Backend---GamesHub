f54aef18edb80bc6bbb08b2b247655e47e4fba09
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-17 13:45:24 +0530

    Updated the error code for auth with 401 - Unauthorized.

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
-  if (!token) return res.status(400).send("Access Denied. No token Provided");
+  if (!token) return res.status(401).send("Access Denied. No token Provided");
   try {
     const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(400).send("Invalid token.");
+    return res.status(401).send("Invalid token.");
   }
 };
