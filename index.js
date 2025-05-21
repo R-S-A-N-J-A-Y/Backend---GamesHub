8f5f7c611b68fd4f11e9ae3a0fbc5965ae58b4a9
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-21 23:32:15 +0530

    Added Pagination for Genre Page. Invoked re-render and query the data based on the page number.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const connectDB = require("./config/dbconfig");
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/admin", require("./routes/adminRoutes"));
 app.use("/user", require("./routes/userRoutes"));
