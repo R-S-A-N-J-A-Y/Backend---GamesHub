f2396776725d234dfc2fadea6a6ecc2b15f18f97
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-07-06 21:00:50 +0530

    implemented an endpoint for token validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 const routes = express.Router();
 
 const AuthController = require("../auth/authHandler");
+const auth = require("../middleware/auth");
 
 routes.get("/", AuthController.GetAllUser);
+routes.get("/verifyToken", auth, (req, res) => {
+  return res.json({
+    valid: true,
+    user: req.user,
+  });
+});
 routes.post("/login", AuthController.Login);
 routes.post("/register", AuthController.Register);
 
